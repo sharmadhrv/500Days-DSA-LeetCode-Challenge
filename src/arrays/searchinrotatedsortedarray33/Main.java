9d52e1557255696ad27e929012e81a3f85ee9b7d
Dhruv <sharmadhruv0499@gmail.com>
2025-11-17 11:03:13 +0530

    Arrays: Search for DUplicates in Rotated SOrted Array

--- src/arrays/searchinrotatedsortedarray33/Main.java
+++ src/arrays/searchinrotatedsortedarray33/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         RotatedArrSearch rotatedArrSearch = new RotatedArrSearch();
-        int[] nums = {3,1};
-        System.out.println(rotatedArrSearch.search(nums,1));
+        int[] nums = {2,5,6,0,0,1,2};
+        System.out.println(rotatedArrSearch.search(nums,0));
     }
 }
