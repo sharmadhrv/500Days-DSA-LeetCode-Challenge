20e2716c0f7111342b658985410ad0b173204b5f
Dhruv <sharmadhruv0499@gmail.com>
2025-11-17 23:35:02 +0530

    Arrays: SIngle Element in SOrted Array

---
+++ src/arrays/singleelemeninsortedarray540/Main.java
@@ -0,0 +1,11 @@
+package arrays.singleelemeninsortedarray540;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SingleElement singleElement = new SingleElement();
+        int[] nums = {7,7,10,11,11,12,12};
+        System.out.println(singleElement.findSingleElement(nums));
+    }
+}
