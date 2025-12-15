d3ff45b071ba4e4191505847d0e7969e7fb11dee
Dhruv <sharmadhruv0499@gmail.com>
2025-12-15 10:02:53 +0530

    Strings: Phone number letter combination

--- src/strings/lettercombinationofphnnum17/LetterCombinations.java
+++ src/strings/lettercombinationofphnnum17/LetterCombinations.java
@@ -1,47 +1,47 @@
 package strings.lettercombinationofphnnum17;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class LetterCombinations {
 
     public List<String> letterCombinations(String digits) {
 
-        List<String> list = new ArrayList<>() ;
-        Map<Character,String> map  = new HashMap<>();
-            map.put('2',"abc");
-            map.put('3',"def");
-            map.put('4',"ghi");
-            map.put('5',"jkl");
-            map.put('6',"mno");
-            map.put('7',"pqrs");
-            map.put('8',"tuv");
-            map.put('9',"wxyz");
+        List<String> list = new ArrayList<>();
+        Map<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
 
-            char[] digitsArray = digits.toCharArray();
-            List<char[]> alphabetList = new ArrayList<>();
-            for(char c :digits.toCharArray())
+        List<List<String>> answer = new ArrayList<>(2);
+        char firstDigit = digits.charAt(0);
+        String firstDigitAlphabet = map.get(firstDigit);
+        String[] firstDigitArray = firstDigitAlphabet.split("");
+        List<String> innerArray = new ArrayList<>(Arrays.asList(firstDigitArray));
+        answer.add(new ArrayList<>(innerArray));
+        for(int i =1;i<digits.length();i++)
         {
-            alphabetList.add(map.get(c).toCharArray());
+            char digit = digits.charAt(i);
+            innerArray.clear();
+             firstDigitAlphabet = map.get(digit);
+             firstDigitArray = firstDigitAlphabet.split("");
+             List<String> listArray = answer.getFirst();
+             for(int j =0;j< listArray.size();j++)
+             {
+                 for(int k =0;k<firstDigitArray.length;k++)
+                 {
+                     String str = listArray.get(j)+firstDigitArray[k];
+                     innerArray.add(str);
+                 }
+             }
+            answer.add(new ArrayList<>(innerArray));
+            answer.removeFirst();
         }
-            List<String> answer = new ArrayList<>();
-        if(alphabetList.size() ==1)
-        {
-            for(char c : alphabetList.getFirst())
-            {
-                answer.add(String.valueOf(c));
-            }
-        }
-        if(alphabetList.size()==2)
-        {
-            for(char c1 : alphabetList.get(0))
-            {
-                for(char c2 : alphabetList.get(1))
-                    answer.add(c1 +String.valueOf(c2));
-            }
-        }
-        return answer;
+
+        return answer.getFirst();
     }
 }
--- src/strings/lettercombinationofphnnum17/Main.java
+++ src/strings/lettercombinationofphnnum17/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         LetterCombinations combinations = new LetterCombinations();
-        String s = "27";
+        String s = "2";
         List<String> answer = combinations.letterCombinations(s);
         for(String str : answer)
             System.out.print(str+" ");
