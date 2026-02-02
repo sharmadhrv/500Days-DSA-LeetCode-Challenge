877c41622099f94cfcd9a86ad8d37cc4376d196d
Dhruv <sharmadhruv0499@gmail.com>
2026-02-02 21:01:12 +0530

    divisor in order

--- src/miscellaneous/divisiorinorder/DivisorInOrder.java
+++ src/miscellaneous/divisiorinorder/DivisorInOrder.java
@@ -12,6 +12,7 @@ public class DivisorInOrder {
         int[] small = new int[n];
         int[] large = new int[n];
         int s=0,l=0;
+
         for(int i =1;i*i<n;i++)
         {
             if(n%i == 0)
@@ -20,12 +21,15 @@ public class DivisorInOrder {
                 large[l++] = n/i;
             }
         }
+
         int[] ans = new int[s+l];
         int index = 0;
-        for(int i = 0;i<s;i++)
+
+        for(int i = 0; i<s; i++)
         {
             ans[index++] = small[i];
         }
+
         for(int i = l-1;i>=0;i--)
         {
             ans[index++] = large[i];
