c9c6159b626ba0d221bd129acb0407cddb0da5b9
Dhruv <sharmadhruv0499@gmail.com>
2026-02-09 23:17:29 +0530

    Recurrsion : Strings

--- src/recursion/skipsubstring/SkipSubString.java
+++ src/recursion/skipsubstring/SkipSubString.java
@@ -6,8 +6,10 @@ public class SkipSubString {
     {
         if(unProcessedString.isEmpty())
             return"";
+
         if(unProcessedString.startsWith(pattern))
             return skipSubString(pattern,unProcessedString.substring(pattern.length()));
+
         else
             return unProcessedString.charAt(0)+ skipSubString(pattern,unProcessedString.substring(1));
     }
