94690cecfdef7df4d4a047531bc1aa6ac3f5f7f6
Dhruv <sharmadhruv0499@gmail.com>
2026-02-16 22:06:06 +0530

    Strings: ongest character replacement

---
+++ src/strings/longestrepeatedcharacterreplacement424/CharacterReplacement.java
@@ -0,0 +1,15 @@
+package strings.longestrepeatedcharacterreplacement424;
+
+public class CharacterReplacement {
+
+    public int characterReplacement(String s, int k) {
+
+        int left=0,right,maxLength=0,length=0;
+        int maxFrequency = 0;
+        for(right = 0;right<s.length();right++)
+        {
+          
+        }
+        return maxLength;
+    }
+}
---
+++ src/strings/longestrepeatedcharacterreplacement424/Main.java
@@ -0,0 +1,11 @@
+package strings.longestrepeatedcharacterreplacement424;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CharacterReplacement characterReplacement = new CharacterReplacement();
+        String s = "AABA";
+        System.out.println(characterReplacement.characterReplacement(s,0));
+    }
+}
