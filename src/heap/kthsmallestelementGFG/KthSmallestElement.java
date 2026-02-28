3cfb88974e1de7e1ed39948c2c4b569cce97fa8c
Dhruv <sharmadhruv0499@gmail.com>
2026-02-28 23:48:39 +0530

    kth smallest element using priority queue

---
+++ src/heap/kthsmallestelementGFG/KthSmallestElement.java
@@ -0,0 +1,26 @@
+package heap.kthsmallestelementGFG;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthSmallestElement {
+
+    public int kthSMallestElement(int[] arr, int k) {
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(arr.length, Collections.reverseOrder());
+        int i ;
+        for(i = 0;i<k;i++)
+            priorityQueue.add(arr[i]);
+
+        while(i<arr.length)
+        {
+            if(!priorityQueue.isEmpty() && arr[i] < priorityQueue.peek())
+            {
+                priorityQueue.poll();
+                priorityQueue.add(arr[i]);
+            }
+            i++;
+        }
+        return priorityQueue.peek();
+    }
+}
