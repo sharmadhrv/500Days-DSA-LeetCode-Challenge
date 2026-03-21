76b399ccce8b0e9eba0189e895dc1797aede2b28
Dhruv <sharmadhruv0499@gmail.com>
2026-03-21 18:36:09 +0530

    kth largest element in stream

---
+++ src/heap/kthlargestelementinstream703/KthLargest.java
@@ -0,0 +1,23 @@
+package heap.kthlargestelementinstream703;
+
+import java.util.PriorityQueue;
+
+public class KthLargest {
+
+    PriorityQueue<Integer> minHeap ;
+    int k ;
+    public KthLargest(int k, int[] nums) {
+        minHeap = new PriorityQueue<>();
+        this.k=k;
+        for(int i : nums)
+            add(i);
+    }
+
+
+    public int add(int val) {
+      minHeap.add(val);
+      if(minHeap.size()>k)
+          minHeap.poll();
+      return minHeap.peek();
+    }
+}
