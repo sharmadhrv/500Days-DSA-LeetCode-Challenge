63e030cc121f02fa25db64dfc7457ad4e2989d42
Dhruv <sharmadhruv0499@gmail.com>
2026-03-22 22:56:53 +0530

    heap

--- src/heap/findmedianofdatastream295/Main.java
+++ src/heap/findmedianofdatastream295/Main.java
@@ -3,11 +3,10 @@ package heap.findmedianofdatastream295;
 public class Main {
     public static void main(String[] args) {
         MedianFinder medianFinder = new MedianFinder();
-        medianFinder.addNum(1);    // arr = [1]
-        medianFinder.addNum(2);    // arr = [1, 2]
-        System.out.println(medianFinder.findMedian()); // return 1.5 (i.e., (1 + 2) / 2)
-        medianFinder.addNum(3);    // arr[1, 2, 3]
-        System.out.println(medianFinder.findMedian()); // return 2.0
-
+        medianFinder.addNum(1);
+        medianFinder.addNum(2);
+        System.out.println(medianFinder.findMedian());
+        medianFinder.addNum(3);
+        System.out.println(medianFinder.findMedian());
     }
 }
