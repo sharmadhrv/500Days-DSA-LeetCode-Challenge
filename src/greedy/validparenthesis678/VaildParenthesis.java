2a1d43454a9b8a91031a0c9dd39e95b30e30c5aa
Dhruv <sharmadhruv0499@gmail.com>
2026-04-14 23:24:04 +0530

    Greedy: valid parenthesis

---
+++ src/greedy/validparenthesis678/VaildParenthesis.java
@@ -0,0 +1,30 @@
+package greedy.validparenthesis678;
+
+public class VaildParenthesis {
+
+    public boolean checkValidString(String s) {
+
+        int min = 0;
+        int max = 0;
+        for(char c : s.toCharArray())
+        {
+            if(c == '(')
+            {
+                min +=1;
+                max +=1;
+            }
+            else if(c==')')
+            {
+                min -=1;
+                max +=1;
+            }
+            else{
+                min -=1;
+                max +=1;
+            }
+            if(max<0) return false;
+            if(min<0) min = 0;
+        }
+        return true;
+    }
+}
