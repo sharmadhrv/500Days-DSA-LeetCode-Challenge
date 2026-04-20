7a5d3785663b809afb1279b0045dd5639b27752f
Dhruv <sharmadhruv0499@gmail.com>
2026-04-20 23:57:52 +0530

    greedy

---
+++ src/greedy/validparenthesis678/Main.java
@@ -0,0 +1,10 @@
+package greedy.validparenthesis678;
+
+public class Main {
+    public static void main(String[] args) {
+
+        VaildParenthesis vaildParenthesis = new VaildParenthesis();
+        String s = "(*))";
+        System.out.println(vaildParenthesis.checkValidString2(s));
+    }
+}
--- src/greedy/validparenthesis678/VaildParenthesis.java
+++ src/greedy/validparenthesis678/VaildParenthesis.java
@@ -3,6 +3,7 @@ package greedy.validparenthesis678;
 public class VaildParenthesis {
 
     public boolean checkValidString(String s) {
+
         int min = 0;
         int max = 0;
         for(char c : s.toCharArray())
@@ -25,5 +26,28 @@ public class VaildParenthesis {
             if(min<0) min = 0;
         }
         return true;
+    }
+
+    public boolean checkValidString2(String s) {
+        boolean answer = helper(s,0,0);
+        return answer;
+
+    }
+
+    private boolean helper(String s , int index, int count )
+    {
+        if(count<0) return false;
+        else if(index == s.length())
+            return (count == 0);
+        else{
+            if(s.charAt(index) =='(')
+                return helper(s,index+1,count+1);
+            if(s.charAt(index)==')')
+                return helper(s,index+1,count-1);
+
+            return helper(s,index+1,count+1) ||
+                    helper(s,index+1,count-1) ||
+                    helper(s,index+1,count);
+        }
     }
 }
