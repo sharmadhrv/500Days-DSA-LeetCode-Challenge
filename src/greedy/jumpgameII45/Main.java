acd42d652328b2eff32074875fdaa36cc5a3ae7a
Dhruv <sharmadhruv0499@gmail.com>
2026-04-23 23:27:39 +0530

    greedy: jumpgame 2

---
+++ src/greedy/jumpgameII45/Main.java
@@ -0,0 +1,12 @@
+package greedy.jumpgameII45;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JumpGame2 jumpGame = new JumpGame2();
+        int[] nums = {2,3,1,1,4};
+
+        System.out.println(jumpGame.jump(nums));
+    }
+}
