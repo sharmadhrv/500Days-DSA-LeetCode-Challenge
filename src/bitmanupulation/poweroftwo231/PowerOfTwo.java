e955ac5f9208175dc02848261d78f28844f01456
Dhruv <sharmadhruv0499@gmail.com>
2026-04-29 22:59:07 +0530

    powerof 2

--- src/bitmanupulation/poweroftwo231/PowerOfTwo.java
+++ src/bitmanupulation/poweroftwo231/PowerOfTwo.java
@@ -7,6 +7,7 @@ public class PowerOfTwo {
         System.out.println(isPowerOfTwo(-2147483648));
     }
     public static boolean isPowerOfTwo(int n) {
+
         if(n == 0 || n==Integer.MIN_VALUE || n==Integer.MAX_VALUE)
             return false;
         return (n & (n - 1)) ==0;
