67009772e10381100c6b73b02a816821fbe786a5
Dhruv <sharmadhruv0499@gmail.com>
2026-05-17 22:24:54 +0530

    Binary tree: Level order traversal

---
+++ src/trees/leverordertraversal102/LevelOrder.java
@@ -0,0 +1,33 @@
+package trees.leverordertraversal102;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrder {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> list = new LinkedList<>();
+        if(root == null) return list;
+
+        queue.offer(root);
+
+        while(!queue.isEmpty())
+        {
+            int n = queue.size();
+            List<Integer> subList = new LinkedList<>();
+            for(int i = 0;i<n;i++)
+            {
+                if(queue.peek().left!=null)
+                    queue.offer(queue.peek().left);
+                if(queue.peek().right!=null)
+                    queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            list.add(subList);
+        }
+        return list;
+    }
+}
---
+++ src/trees/leverordertraversal102/Main.java
@@ -0,0 +1,21 @@
+package trees.leverordertraversal102;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+
+        root.left = new TreeNode(9);
+
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        LevelOrder obj = new LevelOrder();
+
+        List<List<Integer>> result = obj.levelOrder(root);
+
+        System.out.println(result);
+    }
+}
