9696d6aee398ae1b48af13c812c777d7711e86ac
Dhruv <sharmadhruv0499@gmail.com>
2026-05-20 22:12:01 +0530

    Trees: Diameter of binary tree

--- src/trees/balancedbinarytree110/BalancedBinaryTree.java
+++ src/trees/balancedbinarytree110/BalancedBinaryTree.java
@@ -15,11 +15,15 @@ public class BalancedBinaryTree {
     private int height(TreeNode root)
     {
         if(root == null) return 0;
+
         int left = height(root.left);
         if(left == -1) return -1;
+
         int right = height(root.right);
         if(right== -1) return -1;
+
         if(Math.abs(left-right)>1) return -1;
+
         return 1+ Math.max(left,right);
     }
 
