6d26f0d566f11810659cc1726fc2737adbe70d17
Dhruv <sharmadhruv0499@gmail.com>
2026-05-24 23:40:20 +0530

    trees: symmetric binary tree

--- src/trees/symmetrictree101/TreeNode.java
+++ src/trees/symmetrictree101/TreeNode.java
@@ -5,7 +5,9 @@ public class TreeNode {
     int val;
       TreeNode left;
       TreeNode right;
+
      TreeNode() {}
+
          TreeNode(int val) { this.val = val; }
          TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
