024b4117eec2b1f6e5cf320eaa41490e7fc6e598
Dhruv <sharmadhruv0499@gmail.com>
2026-05-31 23:15:59 +0530

    Trees: Nodes at distance k

--- src/trees/leverordertraversal102/Main.java
+++ src/trees/leverordertraversal102/Main.java
@@ -9,7 +9,7 @@ public class Main {
         root.left = new TreeNode(9);
 
         root.right = new TreeNode(20);
-        root.right.left = new TreeNode(15);
+        root.right.left = new TreeNode(1);
         root.right.right = new TreeNode(7);
 
         LevelOrder obj = new LevelOrder();
